6247832b24922c8a8adda4fc39c7e92aa58ccb62
Henadzi Kalinouski <hkalinouski@eisgroup.com>
2023-07-05 10:14:01 +0300

    improved image preview

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,48 @@ import { useImgToURIWithWatermark } from "./hooks";
 import "./App.css";
 
 function App() {
-  const [initUri, setInitUri] = useState();
+  const [initUri, setInitUri] = useState<string | ArrayBuffer>();
+  const [watermarkText, setWatermarkText] = useState('')
+
   const uri = useImgToURIWithWatermark({
-    fillText: "Hena",
+    fillText: watermarkText,
     src: `${initUri} `,
   });
   useEffect(() => {
     console.log("initUri", initUri);
   }, [initUri]);
 
-  const onChange = (e) => {
-    const file = e.target.files[0];
+  const onTextFieldChange:React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setWatermarkText(e.target.value)
+  }
+
+  const onFileFieldChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const file = e?.target?.files?.[0];
     const fileReader = new FileReader();
     fileReader.onload = () => {
       const srcData = fileReader.result;
-      setInitUri(srcData);
+      if(srcData) {
+        setInitUri(srcData);
+      }
+      
     };
-    fileReader.readAsDataURL(file);
+    if(file) {
+      fileReader.readAsDataURL(new Blob([file]));
+    }
+    
   };
   return (
     <>
-      <input type="file" onChange={onChange} />
+      <div>
+        <label htmlFor="textField">Add watermark text:</label>
+        <input id="textField" type="text" onChange={onTextFieldChange} />
+      </div>
+      <div>
+          <label htmlFor="fileField">Chooze image:</label>
+          <input id="fileField" type="file" onChange={onFileFieldChange} />
+      </div>    
       <img src={uri} alt="" />
+      {/* <img src={initUri} /> */}
     </>
   );
 }
--- src/hooks/useImgToURIWithWatermark.ts
+++ src/hooks/useImgToURIWithWatermark.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 export const useImgToURIWithWatermark = ({
   fillText,
   src,
@@ -6,27 +6,43 @@ export const useImgToURIWithWatermark = ({
   fillText: string;
   src: string;
 }) => {
-  const [uri, setUri] = useState();
+  const [uri, setUri] = useState("");
   const canvas = document.createElement("canvas");
   const context = canvas.getContext("2d");
-  const imageObj = new Image();
+  const imageObj = useMemo(() => new Image(), []);
 
   useEffect(() => {
     imageObj.onload = function () {
-      context.drawImage(imageObj, 0, 0, 200, 200);
-      context.fillStyle = "white";
-      context.textBaseline = "middle";
-      context.font = "50px 'Montserrat'";
-      context.fillText(fillText, 10, 20);
-      var pngUrl = canvas.toDataURL();
-      setUri(pngUrl);
+      const imgHeight = imageObj.naturalHeight;
+      const imgWidth = imageObj.naturalWidth;
+      canvas.height = imgHeight;
+      canvas.width = imgWidth;
+      if (context) {
+        context.drawImage(
+          imageObj,
+          0,
+          0,
+          imgWidth,
+          imgHeight,
+          0,
+          0,
+          imgWidth,
+          imgHeight
+        );
+        context.fillStyle = "white";
+        context.textBaseline = "middle";
+        context.font = "50px 'Montserrat'";
+        context.fillText(fillText, 10, 20);
+      }
+      const imgUrl = canvas.toDataURL();
+      setUri(imgUrl);
     };
-  }, [fillText, src]);
+  }, [imageObj, canvas, context, fillText]);
 
   useEffect(() => {
     imageObj.crossOrigin = "anonymous"; //not necessary, if image hosted on same server
     imageObj.src = src;
-  }, [src]);
+  }, [imageObj, src]);
 
   return uri;
 };
