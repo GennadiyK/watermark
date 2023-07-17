5cb662b5a8d342260ad01f7af3cbaecbec43fe20
Henadzi Kalinouski <hkalinouski@eisgroup.com>
2023-07-17 14:30:08 +0300

    add github link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import logo from '../../assets/WATERMSRK.png'
-import './header.css'
-
+import React from "react";
+import logo from "../../assets/WATERMSRK.png";
+import githubIcon from "../../assets/GitHub_Logo.png";
+import "./header.css";
 
 export const Header = () => {
-    return (
-        <header className='header'>
-            <img src={logo} alt="Watermark" height="120px" className='header-img'/>
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <img src={logo} alt="Watermark" height="120px" className="header-img" />
+      <div className="header-inner">
+        
+        <a
+          className="header-link"
+          href="https://github.com/GennadiyK/watermark"
+          target="_blank"
+          title="Open GitHub repository"
+        >
+          &#11088;
+          <img src={githubIcon} alt="Github repository" width="70" />
+        </a>
+      </div>
+    </header>
+  );
+};
